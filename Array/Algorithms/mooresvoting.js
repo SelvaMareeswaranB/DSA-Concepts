75123b3e8d7e935292d4d50c073c27052483eaa2
Selva Mareeswaran <selvamareeswaranb@gmail.com>
2025-03-27 20:29:04 +0530

    moores voting algorithm implemented

---
+++ Array/Algorithms/mooresvoting.js
@@ -0,0 +1,25 @@
+var majorityElement = function (nums) {
+  let length = nums.length;
+  let element;
+  let count = 0;
+  for (let i = 0; i < length; i++) {
+    if (count === 0) {
+      count = 1;
+      element = nums[i];
+    } else if (nums[i] === element) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  let frequency = 0
+  for(let i=0;i<length;i++){
+    nums[i] === element ? frequency++ : ""
+  }
+  let result=frequency*2 > length ? element : -1
+  return result
+};
+
+let nums = [2,2,1,1,1,2,2]
+console.log(majorityElement(nums))
---
+++ Array/leetcode/minOperations.js
@@ -0,0 +1,25 @@
+var minOperations = function (grid, x) {
+  let flatGrid = grid.flat();
+
+  flatGrid.sort((a, b) => a - b);
+  console.log(flatGrid);
+  let length = flatGrid.length - 1;
+  let medianIndex = Math.floor(flatGrid.length / 2);
+  let target = flatGrid[medianIndex]; 
+  let operations = 0;
+  for (let i = 0; i <= length; i++) {
+
+    let differnece = Math.abs(flatGrid[i] - target);
+  
+    if (differnece % x !== 0) return -1;
+  
+    let temp = differnece / x;
+    operations = operations + temp;
+  }
+  return operations;
+};
+
+let grid =[[2, 6], [10, 14]]
+let x = 4;
+
+console.log(minOperations(grid, x));
