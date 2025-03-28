75a4bff3cab089820940bd0fe330b7635ee5801b
Selva Mareeswaran <selvamareeswaranb@gmail.com>
2025-03-28 20:34:58 +0530

    problems implemented

---
+++ Array/leetcode/applyOperations.js
@@ -0,0 +1,17 @@
+var applyOperations = function (nums) {
+  let l = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1] &&i < nums.length - 1) {
+        nums[i] = nums[i] * 2;
+        nums[i + 1] = 0;
+      }
+    if (nums[i] !== 0) {
+      [nums[i], nums[l]] = [nums[l], nums[i]];
+      l++;
+    }
+  }
+  return nums;
+};
+
+console.log(applyOperations([0,1]));
---
+++ Array/leetcode/moveZero.js
@@ -0,0 +1,12 @@
+var moveZeroes = function (nums) {
+  let l = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[i], nums[l]] = [nums[l], nums[i]];
+      l++;
+    }
+  }
+  return nums;
+};
+
+console.log(moveZeroes([1, 0]));
